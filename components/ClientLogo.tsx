f9024245146f7edbd8a8154f056a2fb8acda51d9
Yusuf Ali <isaihmed09@gmail.com>
2025-07-12 19:30:39 +0600

    adding some motion animation

--- components/ClientLogo.tsx
+++ components/ClientLogo.tsx
@@ -1,3 +1,6 @@
+"use client"
+import {easeIn, motion} from "motion/react"
+
 import LogoA from "@/public/assets/logoa.png"
 import LogoB from "@/public/assets/logob.png"
 import LogoD from "@/public/assets/logod.png"
@@ -7,13 +10,30 @@ import Image from "next/image"
 
 function ClientLogo() {
   return (
-    <div className="responsive-container flex gap-4 md:gap-10 items-center justify-center mt-4 md:mt-0 pb-10">
+    <motion.div 
+    initial={{
+      opacity:0,
+      scale:0
+    }}
+
+    animate={{
+      opacity:1,
+      scale:1
+    }}
+
+    transition={{
+      ease: easeIn,
+      duration: 0.5
+    }}
+
+    
+    className="responsive-container flex gap-4 md:gap-10 items-center justify-center mt-4 md:mt-0 pb-10">
       <Image className="logo-image" src={LogoA} alt="logo a"/>
       <Image className="logo-image" src={LogoB} alt="logo b"/>
       <Image className="logo-image" src={LogoD} alt="logo d"/>
       <Image className="logo-image" src={LogoF} alt="logo f"/>
       <Image className="logo-image" src={LogoG} alt="logo g"/>
-    </div>
+    </motion.div>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Image from "next/image"
 import heroImage from "@/public/hero-img.svg"
 import { Caladea, Mulish } from "next/font/google";
 import ActionButton from "./ActionButton";
+import {easeIn, easeOut, motion} from "motion/react";
 
 const caladea = Caladea({
   subsets: ["latin"],
@@ -21,15 +24,72 @@ function Hero() {
       <div className="responsive-container md:flex md:flex-row md:gap-2 md:h-[500px] lg:h-[600px] items-center">
 
       <div className="md:w-[60%] flex flex-col gap-4 md:gap-8">
-        <h1 className={`text-[#2F2E41] text-4xl sm:text-5xl md:text-5xl lg:text-6xl text-center md:text-start mt-4 pt-10 md:pt-0 md:mt-0 ${caladea.className} text-bold`}>Grow Your Business with Results-Driven Digital Marketing</h1>
-        <h5 className={`${mulish.className} text-lg text-center md:text-start`}>We help startups and small businesses increase traffic, leads, and sales through tailored digital strategies</h5>
-        <div className="flex flex-col md:flex-row gap-4 items-center">
+        <motion.h1 
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: easeIn
+        }}
+        className={`text-[#2F2E41] text-4xl sm:text-5xl md:text-5xl lg:text-6xl text-center md:text-start mt-4 pt-10 md:pt-0 md:mt-0 ${caladea.className} text-bold`}>Grow Your Business with Results-Driven Digital Marketing</motion.h1>
+        <motion.h5 
+        initial={{
+          x: "-100%",
+          opacity: 0
+        }}
+        animate={{
+          x: 0,
+          opacity: 1
+        }}
+        transition={{
+          duration: 0.5,
+          ease: easeIn
+        }}
+        className={`${mulish.className} text-lg text-center md:text-start`}>We help startups and small businesses increase traffic, leads, and sales through tailored digital strategies</motion.h5>
+        <motion.div
+        initial={{
+          y: "-100%",
+          opacity: 0
+        }}
+        animate={{
+          y: 0,
+          opacity: 1
+        }}
+        transition={{
+          duration: 0.6,
+          ease: easeIn
+        }}
+        className="flex flex-col md:flex-row gap-4 items-center">
           <ActionButton/>
           <a className={`${mulish.className} border-b border-[#DDEB9D] hover:border-[#a0ac6a]`} href="#portfolio">Scrolls to portfolio</a>
-        </div>
+        </motion.div>
       </div>
 
-      <Image className="md:w-[40%] md:h-[22rem] pt-10 md:pt-0 m-auto"  src={heroImage} alt="Hero Image" />
+
+      <motion.div
+      initial={{
+        x: 100,
+        scale: 3,
+        opacity:0
+      }}
+      animate={{
+        x: 0,
+        scale: 1,
+        opacity:1
+      }}
+      transition={{
+        ease: easeOut,
+        duration: 0.8
+      }}
+      className="md:w-[40%] md:h-[22rem] pt-10 md:pt-0 m-auto"
+      >
+        <Image 
+        className="w-full h-full"  src={heroImage} alt="Hero Image" />
+      </motion.div>  
       
     </div>
     </div>
